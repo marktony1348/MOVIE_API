ed6dddc1a2925b3106306ff8e86f641f49138b4d
Udennaka Marktony <chukwudiudennaka@gmail.com>
2022-02-15 00:05:47 +0100

    update on index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Directors = Models.Director
 //   { useNewUrlParser: true, useUnifiedTopology: true });
 
 // online database
-mongoose.connect('process.env.MONGO_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.MONGO_URL', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
   morgan = require('morgan');

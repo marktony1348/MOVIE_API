06c7e18ac14f982d74b4651778f8ff08ba3a1730
Udennaka Marktony <chukwudiudennaka@gmail.com>
2022-02-25 08:23:11 +0100

    changed authentication on /movies endpoint

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // refactored domain access
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);

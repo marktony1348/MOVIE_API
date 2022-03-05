df05f59ad2a40ff88481df7acb31225192261263
Udennaka Marktony <chukwudiudennaka@gmail.com>
2022-03-05 15:58:28 +0100

    update removal of jwt

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ app.get('/documentation', (req, res) => {
 });
 
 // brings a json request on routing (ALL MOVIES)
-app.get('/movies', passport.authenticate('jwt', { session: false }),  (req, res) => {
+app.get('/movies',  (req, res) => {
   Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
